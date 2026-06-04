724c97b70e1f1b3c7bc418fe6124d164a96db724
Andrei Kuminov <akumidv@yandex.ru>
2026-06-04 14:15:15 +1000

    naming

--- src/options_assembler/analytic/__init__.py
+++ src/options_assembler/analytic/__init__.py
@@ -1,6 +1,6 @@
 """Public analytics module functions"""
 
-from options_assembler.analytic.analityc_class import OptionAnalytic
+from options_assembler.analytic.analytic_class import OptionAnalytic
 from options_assembler.analytic import risk
 from options_assembler.analytic import price
 
